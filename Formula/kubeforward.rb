5177560cbab6caf1148216e5fcf53040b4295e01
Fernando Crozetta <8006246+fcrozetta@users.noreply.github.com>
2026-02-26 16:16:16 +0000

    set once to work with PR

--- Formula/kubeforward.rb
+++ Formula/kubeforward.rb
@@ -1,8 +1,8 @@
 class Kubeforward < Formula
   desc "Port-forward Kubernetes services/pods with a friendly workflow"
   homepage "https://github.com/fcrozetta/kubeforward"
-  version "0.0.2"
-  license "Apache-2.0" # TODO: set correctly
+  version "0.0.3"
+  license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
@@ -14,23 +14,23 @@ class Kubeforward < Formula
   end
 
   on_linux do
-  if Hardware::CPU.intel?
-    url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-amd64.tar.gz"
-    sha256 "b4c7064a3dba5676e001ccd4030f5ed8eabd157d5abf91b8a0df65c9bb396f92"
-  elsif Hardware::CPU.arm?
-    url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-arm64.tar.gz"
-    sha256 "49c4a1928084b7544c2e2dbbd99c2071fbefe09bfa8d28f4519bc2ae385e6f9e"
+    if Hardware::CPU.intel?
+      url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-amd64.tar.gz"
+      sha256 "b4c7064a3dba5676e001ccd4030f5ed8eabd157d5abf91b8a0df65c9bb396f92"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-arm64.tar.gz"
+      sha256 "49c4a1928084b7544c2e2dbbd99c2071fbefe09bfa8d28f4519bc2ae385e6f9e"
+    else
+      odie "kubeforward is not released for this linux architecture yet"
+    end
   end
-end
 
   def install
     bin.install "kubeforward"
   end
 
   test do
-    # Pick a flag your CLI guarantees.
-    # If you don't print version, change to "--help" with a stable match.
-    out = shell_output("#{bin}/kubeforward --help")
+    out = shell_output("#{bin}/kubeforward --version")
     assert_match version.to_s, out
   end
 
