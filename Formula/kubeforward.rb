f0c762f3ab5aaafcba395fc0492103488598bd58
Fernando Crozetta <8006246+fcrozetta@users.noreply.github.com>
2026-02-26 15:07:57 +0000

    updated kubeforward to 0.0.3

--- Formula/kubeforward.rb
+++ Formula/kubeforward.rb
@@ -7,7 +7,7 @@ class Kubeforward < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-darwin-arm64.tar.gz"
-      sha256 "ff1329ce27d5d4317ad17e69bb62a1b72a197583f1fe68385d50ad082325d008"
+      sha256 "c8a870121e5dbd694d54267621bb199b9b0a19562b91fe7677be64590dbfbb94"
     else
       odie "kubeforward is not released for darwin-amd64 yet"
     end
@@ -16,10 +16,10 @@ class Kubeforward < Formula
   on_linux do
   if Hardware::CPU.intel?
     url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-amd64.tar.gz"
-    sha256 "<PLEASE_UPDATE_ME>"
+    sha256 "b4c7064a3dba5676e001ccd4030f5ed8eabd157d5abf91b8a0df65c9bb396f92"
   elsif Hardware::CPU.arm?
     url "https://github.com/fcrozetta/kubeforward/releases/download/#{version}/kubeforward-#{version}-linux-arm64.tar.gz"
-    sha256 "<PLEASE_UPDATE_ME>"
+    sha256 "49c4a1928084b7544c2e2dbbd99c2071fbefe09bfa8d28f4519bc2ae385e6f9e"
   end
 end
 
